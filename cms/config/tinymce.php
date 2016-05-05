ab0b7a6c1f4c00db5accee74f62817ba95d1a573
yusukesasaki <yusukesasaki.com@gmail.com>
2016-05-06 00:40:53 +0900

    TinyMCE導入によるartisanのエラー解消

--- cms/config/tinymce.php
+++ cms/config/tinymce.php
@@ -2,7 +2,7 @@
 
 return [
 
-	'cdn' => url('vendor/js/tinymce/tinymce.min.js'),
+  'cdn' => app()->runningInConsole() ? null : url('vendor/js/tinymce/tinymce.min.js'),
 
 	// 'default' => [
 	// 	"selector" => ".tinymce",
